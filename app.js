844dc21a094a300cbf322cc2d34f98716d9ecc6e
KiaKaramy <kiakaramy11@gamil.com>
2025-10-15 19:37:38 +0330

    change the logic of server run

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
-const Verification = require("./src/router/Verification.router");
-const dotenv = require("dotenv");
-const { UserServerStartConection } = require("./src/config/mongo-users");
-const configs = require("./src/config/env");
-dotenv.config();
 const app = express();
+const Verification = require("./src/router/Verification.router")
+const cookieParser = require("cookie-parser")
 
 // const {GetRegister} = require("")
 app.use(express.json());
@@ -18,10 +14,4 @@ app.get("/" , (req , res)=>{
     res.json({messge : "fuck you"})
 })
 
-
-UserServerStartConection()
-    .then(() => console.log("MongoDB connected!"))
-    .catch((err) => console.error("MongoDB connection error:", err));
-
-    
-module.exports =  app;
+module.exports = app;
