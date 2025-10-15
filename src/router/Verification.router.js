fe3a73e8890369577acdf2c66f43ccc6ca1cc2b7
KiaKaramy <kiakaramy11@gamil.com>
2025-10-15 20:56:29 +0330

    error handel

--- src/router/Verification.router.js
+++ src/router/Verification.router.js
@@ -4,6 +4,11 @@ const {sendCodeController , checkCode} = require("../controller/verify.controlle
 
 Verification.post("/sendcode" , /*send code*/ sendCodeController);
 Verification.post("/checkcode" , /*check code*/ checkCode);
-
+Verification.get("/kia" , (req , res)=>{
+    res.json({message : "kia"})
+})
+Verification.post("/nima" , (req , res)=>{
+    res.json({message : "nima"})
+})
 
 module.exports =  Verification;
